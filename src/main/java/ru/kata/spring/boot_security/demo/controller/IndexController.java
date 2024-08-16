b6fab0c222c0a446d2ddc2e48b300da10e4e2d81
CoreFrisk <martakova_aa@list.ru>
2024-08-17 00:54:52 +0300

    cherry picked from commit 04d6986a477d041938098976856397db7bfcdaaf

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/IndexController.java
@@ -0,0 +1,11 @@
+package ru.kata.spring.boot_security.demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String makeHi() {return "index";}
+}
