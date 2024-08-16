62ccaa750ae50b840c560fefb359656ea68a5486
CoreFrisk <martakova_aa@list.ru>
2024-08-17 00:44:23 +0300

    Zhest cho ya tyt ponadabavlyaalaaa aaaa Vrema 1 nochi

--- src/main/java/ru/kata/spring/boot_security/demo/Repositories/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/Repositories/UserRepository.java
@@ -2,7 +2,6 @@ package ru.kata.spring.boot_security.demo.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.models.User;
 
@@ -13,5 +12,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.username = :username")
-    User getUserByUsername(String username);
+    Optional <User> getUserByUsername(String username);
 }
