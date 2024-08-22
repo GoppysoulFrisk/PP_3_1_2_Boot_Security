d75a2cb653e6129da6408b0a6d1f5527a092f212
CoreFrisk <martakova_aa@list.ru>
2024-08-22 23:55:22 +0300

    add some staff on js

--- src/main/java/ru/kata/spring/boot_security/demo/controller/PageController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/PageController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-//@CrossOrigin(origins = {"http://localhost:8080"})
 public class PageController {
 
     @GetMapping
